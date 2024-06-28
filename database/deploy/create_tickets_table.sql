7f871e725f2e57412e5b87970f93a9a9bf146b01
hadi-darwish <hadi.darwish.03@gmail.com>
2024-06-28 12:51:34 +0300

    hotfix(database):fix primary key

--- database/deploy/create_tickets_table.sql
+++ database/deploy/create_tickets_table.sql
@@ -4,14 +4,13 @@ BEGIN;
 
 -- XXX Add DDLs here.
 CREATE TABLE IF NOT EXISTS evently.tickets (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   price DOUBLE PRECISION NOT NULL,
   quantity INT NOT NULL,
   events_id INT NOT NULL,
   ticket_types_id INT NOT NULL,
   created_at TIMESTAMPTZ NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NULL DEFAULT NOW(),
-  PRIMARY KEY (id, events_id, ticket_types_id),
   FOREIGN KEY (events_id) REFERENCES evently.events(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
   FOREIGN KEY (ticket_types_id) REFERENCES evently.ticket_types(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
